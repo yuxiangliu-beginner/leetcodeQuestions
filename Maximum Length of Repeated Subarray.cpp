d8e40e839c72706dbd47dc45e2d16f121e13c76a
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-07-08 14:20:13 -0700

    MaxLengthOfRepeatSubarray

---
+++ Maximum Length of Repeated Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+//     https://leetcode.com/problems/maximum-length-of-repeated-subarray/discuss/109068/JavaC%2B%2B-Clean-Code-8-lines
+public:
+    int findLength(vector<int>& a, vector<int>& b) {
+        int m = a.size(), n = b.size();
+        if (!m || !n) return 0;
+        vector<int> dp(n + 1);
+        int res = 0;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                res = max(res, dp[j] = a[i] == b[j] ? 1 + dp[j + 1] : 0);
+            }
+        }
+        return res;
+    }
+};
