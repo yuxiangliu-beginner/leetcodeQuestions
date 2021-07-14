02be179b397bce2855fe7420fb88b2b5d4e5d5f5
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-07-14 13:46:11 -0700

    RobootBoundedInCircle

---
+++ Robot Bounded In Circle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+//     https://leetcode.com/problems/robot-bounded-in-circle/discuss/290856/JavaC%2B%2BPython-Let-Chopper-Help-Explain
+public:
+    bool isRobotBounded(string instructions) {
+       int x = 0, y = 0 , i = 0;
+        vector<vector<int>> d = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        for(char & ins: instructions)
+        {
+            if(ins=='R')
+            {
+                i= (i+1) %4;
+            }else if(ins == 'L')
+            {
+                i = (i+3) %4;
+            }
+            else
+            {
+                x += d[i][0];
+                y += d[i][1];
+            }
+        }
+        return x==0 && y == 0 || i > 0;
+    }
+};
