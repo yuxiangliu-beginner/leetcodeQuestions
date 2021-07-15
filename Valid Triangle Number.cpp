de28effc7d03527a824d52eb36db75d93be09e85
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-07-15 13:56:24 -0700

    ValidTriangleNumber

---
+++ Valid Triangle Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+//     https://leetcode.com/problems/valid-triangle-number/discuss/1339340/C%2B%2BJavaPython-Two-Pointers-Picture-Explain-Clean-and-Concise-O(N2)
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size(), ans = 0;
+        for(int k = 2; k < n;++k){
+            int i = 0 , j = k-1;
+            while(i < j)
+            {
+                if(nums[i]+nums[j] > nums[k])
+                {
+                    ans+= (j-i);
+                    j-=1;
+                }else
+                {
+                    i+=1;
+                }
+            }
+        }
+        return ans;
+    }
+};
