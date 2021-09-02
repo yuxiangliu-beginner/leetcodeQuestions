7810214d44433e48bd3e46998d2cbbaa69a61043
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-09-02 10:13:28 -0700

    UniqueBinarySearchTreesII

--- Unique Binary Search Trees II.cpp
+++ Unique Binary Search Trees II.cpp
@@ -41,3 +41,30 @@ public:
         
     }
 };
+
+// https://www.cnblogs.com/grandyang/p/4301096.html
+// class Solution {
+// public:
+//     vector<TreeNode*> generateTrees(int n) {
+//         if (n == 0) return {};
+//         vector<vector<vector<TreeNode*>>> memo(n, vector<vector<TreeNode*>>(n));
+//         return helper(1, n, memo);
+//     }
+//     vector<TreeNode*> helper(int start, int end, vector<vector<vector<TreeNode*>>>& memo) {
+//         if (start > end) return {nullptr};
+//         if (!memo[start - 1][end - 1].empty()) return memo[start - 1][end - 1];
+//         vector<TreeNode*> res;
+//         for (int i = start; i <= end; ++i) {
+//             auto left = helper(start, i - 1, memo), right = helper(i + 1, end, memo);
+//             for (auto a : left) {
+//                 for (auto b : right) {
+//                     TreeNode *node = new TreeNode(i);
+//                     node->left = a;
+//                     node->right = b;
+//                     res.push_back(node);
+//                 }
+//             }
+//         }
+//         return memo[start - 1][end - 1] = res;
+//     }
+// };
