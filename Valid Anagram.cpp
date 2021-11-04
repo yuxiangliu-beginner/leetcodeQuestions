32a96b91487f948d4d1cf0f09e082fb5ee41a1cc
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-11-04 11:21:44 -0700

    ValidAnagram

---
+++ Valid Anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int s1 =0 , s2 = 0;
+        if(s.length()!= t.length())
+        {
+            return false;
+        }
+        int n = s.length();
+        int counts[26] = {0};
+        
+        for(int i = 0 ; i<s.length();i++)
+        {   
+            counts[s[i]-'a'] ++;
+            counts[t[i]-'a'] --;
+            
+        }
+        for(int i = 0 ; i < 26 ;i++)
+        {
+            if(counts[i])
+                return false;
+        }
+        
+        return true;
+    }
+};
