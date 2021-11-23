042a33f85eae2675e9e78f5ae215cdc02895de66
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-11-22 22:24:30 -0800

    UnionSet

---
+++ UnionSet.cpp
@@ -0,0 +1,38 @@
+class UnionSet{
+public:
+	UnionSet(int n )
+	{
+		rank_ = vector<int>(n+1,0);
+		parents_ = vector<int>(n+1,0);	
+		for(int i =0 ; i <= n;i++)
+		{
+			parents_[i] = i;
+		}
+	}
+	bool union(int u ,int v)
+	{
+		int pu = Find(u);
+		int pv = Find(v);
+
+		if(pu == pv)
+			return false;
+		if(rank_[pv] < rank_[pu])
+			parents_[pv] = pu;
+		else if(rank_[pu]< rank_[pv])
+			parents_[pu] = pv;
+		else{
+			parents_[pv] =pu;
+			rank_[pu]++;
+		}
+		return true;
+	}
+	int Find(int u)
+	{
+		if(u!=parents_[u])
+			parents_[u] = Find(parents_[u]);
+		return parents_;
+	}
+private:
+	vector<int> parents_;
+	vector<int> rank_;
+}
