2d27625609121118a40c77b0eb475d5f50991c12
Yuxiang Liu <liuyuxiang309@gmail.com>
2021-12-03 07:20:53 -0800

    MaximumProductSubarray

--- Maximum Product Subarray.cpp
+++ Maximum Product Subarray.cpp
@@ -2,14 +2,20 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size(), res = nums[0], l = 0, r = 0;
-        for(int i =0 ; i < n; i++)
+        int n =nums.size();
+        if(n== 1)
+            return nums[0];
+        int l = 0;
+        int r = 0;
+        int res = INT_MIN;
+        
+        for(int i = 0 ; i < n;i++)
         {
             l = (l?l:1) * nums[i];
             r = (r?r:1) * nums[n-1-i];
+            cout<<l<<" "<<r<<endl;
             res = max(res,max(l,r));
         }
         return res;
-        
     }
 };
